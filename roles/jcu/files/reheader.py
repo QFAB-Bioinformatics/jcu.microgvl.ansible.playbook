58d69db3163b20ad43d0a9270bc7cfbe91d28e5d
mthang <m.thang@qfab.org>
2017-05-25 14:28:51 +1000

    Add files via upload

--- roles/jcu/files/reheader.py
+++ roles/jcu/files/reheader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python 
 import sys
-#from Bio.Seq import Seq
+from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from os.path import basename
@@ -11,22 +11,59 @@ filename=sys.argv[1]
 infile = sys.argv[2]
 str_to_add = os.path.splitext(basename(filename))[0]
 outfile = sys.argv[3]
+outdir = sys.argv[4]
+logfile = sys.argv[5]
+
+rdict = {
+    '_R1': '/1',
+    '_R2': '/2',
+    '_1': '/1',
+    '_2': '/2',
+}
+
+rdict_remove = {
+    '_R1': '',
+    '_R2': '',
+    '_1': '',
+    '_2': '',
+}
 
 def makesubs(s):
     for pattern, repl in rdict.iteritems():
         s = re.sub(pattern, repl,s)
     return s
 
-str_to_add = makesubs(str_to_add)
+def makesubs_remove(s):
+    for pattern, repl in rdict_remove.iteritems():
+        s = re.sub(pattern, repl,s)
+    return s
 
+def appendStringToSequenceHeader(inputfile,header_to_add):
+    records=[]
+    for seq_record in SeqIO.parse(inputfile, "fastq"):
+        header =seq_record.id
+        header = "{0}".format(header) + "_" +header_to_add
+        record = SeqRecord(seq_record.seq,id=header,description="")
+        record.letter_annotations["phred_quality"]=seq_record.letter_annotations["phred_quality"]
+        records.append(record)
+    return records
+
+str_to_search = makesubs_remove(str_to_add)
+str_to_add = makesubs(str_to_add)
 final_records=[]
-for seq_record in SeqIO.parse(infile, "fastq"):
-    #print seq_record.format("fastq")
-    #read header
-    header =seq_record.id
-    #add /1 at the end
-    header = "{0}".format(header) + "_" +str_to_add
-    record = SeqRecord(seq_record.seq,id=header,description="")
-    record.letter_annotations["phred_quality"]=seq_record.letter_annotations["phred_quality"]
-    final_records.append(record)
+outlogfile=open(os.path.join(outdir,logfile),"w")
+
+#if metadatafile:
+#    metafile = open(metadatafile,"r")
+#    for line in metafile:
+#        if re.search(str_to_search,line):
+#            outlogfile.write(line)
+                
+#    final_records=appendStringToSequenceHeader(infile,str_to_add)
+#    SeqIO.write(final_records, outfile , "fastq")
+#    outlogfile.close()
+#else:
+final_records=appendStringToSequenceHeader(infile,str_to_add)
+outlogfile.write(str_to_search)
 SeqIO.write(final_records, outfile , "fastq")
+outlogfile.close()
